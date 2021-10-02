718a9cb5f5a926ac0f250738b8b6e1bf32f09cad
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2021-10-02 16:41:29 -0300

    Finalizado o projeto com testes, hooks e prop-types

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,3 +1,54 @@
+import { useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import narutoImg from '../../images/naruto.png';
+import { Quotes } from '../../components';
+import { getQuote } from '../../services';
+import jutsoSound from '../../sounds/jutso.mp3';
+
+const audio = new Audio(jutsoSound);
+
 export function App() {
-  return <h1>Hello</h1>;
+  const isMounted = useRef(true);
+
+  const [quoteState, setQuoteState] = useState({
+    quote: 'loading quote...',
+    speaker: 'loading speaker...',
+  });
+
+  const onUpdate = async () => {
+    const quote = await getQuote();
+
+    if (isMounted.current) {
+      audio.play();
+      setQuoteState(quote);
+    }
+  };
+
+  useEffect(() => {
+    onUpdate();
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  return (
+    <Content>
+      <Quotes {...quoteState} onUpdate={onUpdate} />
+      <NarutoImg src={narutoImg} alt="Naruto with kunai" />
+    </Content>
+  );
 }
+
+const Content = styled.div`
+  height: 100vh;
+  padding: 0 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NarutoImg = styled.img`
+  max-width: 50vw;
+  align-self: flex-end;
+`;
